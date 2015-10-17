a9221ea9778eda7ef3c57f3a220cf997069b2cf3
Maily Santos <mailysantos@gmail.com>
2015-10-17 00:10:28 -0300

    correcoes no banco de dados

--- src/db/database.c
+++ src/db/database.c
@@ -22,7 +22,7 @@ int connect_db(){
 }
 
 int create_table() {
-    sql_create = "CREATE TABLE IF NOT EXISTS cliente (id INTEGER PRIMARY KEY ASC NOT NULL, user_id INTEGER, birthday VARCHAR[12],link VARCHAR[100], name VARCHAR(100), locale VARCHAR[100], gender VARCHAR[10], email VARCHAR[80]);";
+    sql_create = "CREATE TABLE cliente (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, user_id TEXT NOT NULL, birthday TEXT NOT NULL, link TEXT NOT NULL, name TEXT NOT NULL, location TEXT NOT NULL, gender TEXT NOT NULL, email TEXT NOT NULL);";
     int r = connect_db();
 	if (r != SQLITE_OK) {
         
@@ -71,14 +71,18 @@ int insert_row(cliente c) {
             return 0;
         } else {
                   sqlite3_bind_null(stmt, 1);
-                  sqlite3_bind_int(stmt, 2, c.user_id);
+                  sqlite3_bind_text(stmt, 2, c.user_id, strlen(c.user_id), 0);
                   sqlite3_bind_text(stmt, 3, c.birthday, strlen(c.birthday), 0);
                   sqlite3_bind_text(stmt, 4, c.link, strlen(c.link), 0);
                   sqlite3_bind_text(stmt, 5, c.name, strlen(c.name), 0);
-                  sqlite3_bind_text(stmt, 6, c.locale, strlen(c.locale), 0);
+                  sqlite3_bind_text(stmt, 6, c.location, strlen(c.location), 0);
                   sqlite3_bind_text(stmt, 7, c.gender, strlen(c.gender), 0);
                   sqlite3_bind_text(stmt, 8, c.email, strlen(c.email), 0);
         }
+
+        // commit
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
 
         close_db();
         return 1;
@@ -86,7 +90,7 @@ int insert_row(cliente c) {
 }
 
 int update_row(cliente c) {
-    sql_update = "UPDATE cliente SET birthday = ?, link = ?, name = ?, locale = ?, gender = ?, email = ? WHERE user_id = ?;";
+    sql_update = "UPDATE cliente SET birthday = ?, link = ?, name = ?, location = ?, gender = ?, email = ? WHERE user_id = ?;";
 
     int r = connect_db();
     if (r != SQLITE_OK) {
@@ -108,11 +112,15 @@ int update_row(cliente c) {
                     sqlite3_bind_text(stmt, 1, c.birthday, strlen(c.birthday), 0);
                     sqlite3_bind_text(stmt, 2, c.link, strlen(c.link), 0);
                     sqlite3_bind_text(stmt, 3, c.name, strlen(c.name), 0);
-                    sqlite3_bind_text(stmt, 4, c.locale, strlen(c.locale), 0);
+                    sqlite3_bind_text(stmt, 4, c.location, strlen(c.location), 0);
                     sqlite3_bind_text(stmt, 5, c.gender, strlen(c.gender), 0);
                     sqlite3_bind_text(stmt, 6, c.email, strlen(c.email), 0);
-                    sqlite3_bind_int(stmt, 7, c.user_id);
+                    sqlite3_bind_text(stmt, 7, c.user_id, strlen(c.user_id),0);
         }
+
+        // commit
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
 
         close_db();
         return 1;
@@ -145,7 +153,7 @@ int delete_row(int user_id) {
     return 1;
 }
 
-int user_exists(int user_id) {
+int user_exists(char user_id[30]) {
     sql_query_client = "SELECT * FROM cliente WHERE user_id = ?;";
     int r = connect_db();
 	if (r != SQLITE_OK) {
@@ -158,7 +166,7 @@ int user_exists(int user_id) {
     } else{
 
 			r = sqlite3_prepare_v2(conn, sql_query_client, -1, &stmt, 0);
-            sqlite3_bind_int(stmt, 1, user_id);
+            sqlite3_bind_text(stmt, 1, user_id, strlen(user_id),0);
 			    
             int step = sqlite3_step(stmt);
 			if (step == SQLITE_ROW) {
