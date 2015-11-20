91de27595be8b8e1d4730ff4a5e68ef56442b830
Ivan miranda <ivan.i.n@hotmail.com>
2015-11-20 15:39:51 -0200

    ap iniciando como servico

--- src/network/ap.c
+++ src/network/ap.c
@@ -3,8 +3,8 @@
 int setup_ap(){
     system("apt-get install hostapd");
     system("service network-manager stop");
-    system("ifconfig eth0 192.168.0.1");
-    system("ifconfig eth0 up");
-    system("hostapd /etc/hostapd/hostapd.conf");
+    system("ifconfig wlan0 192.168.0.1");
+    system("ifconfig wlan0 up");
+    system("service hostapd /etc/hostapd/hostapd.conf");
     return 1;
 }
