2dbdb360d6a2c47439da46656419d152df3d5256
DenisWP <denis.dabliu@gmail.com>
2015-11-25 21:11:47 -0200

    em criação e testes

--- src/network/iptables.c
+++ src/network/iptables.c
@@ -1,13 +1,18 @@
 #include "iptables.h"
 
 int setup_iptables(){
-    return 1;
+	/*Permitindo pedidos DHCP e DNS de qualquer usuário*/
+	system("iptables -t nat -A prerouting_rule -m state --state NEW,ESTABLISHED,RELATED,INVALID -p udp --dport 67 -j ACCEPT");
+	system("iptables -t nat -A prerouting_rule -m state --state NEW,ESTABLISHED,RELATED,INVALID -p tcp --dport 67 -j ACCEPT");
+	system("iptables -t nat -A prerouting_rule -m state --state NEW,ESTABLISHED,RELATED,INVALID -p udp --dport 53 -j ACCEPT");
+	system("iptables -t nat -A prerouting_rule -m state --state NEW,ESTABLISHED,RELATED,INVALID -p tcp --dport 53 -j ACCEPT");
+	return 1;
 }
 
 int deny_mac(const char* mac){
-    return 1;
+    return iptables_command("iptables -t nat -D prerouting_rule -m mac --mac-source %s -j RETURN",mac);
 }
 
 int allow_mac(const char* mac){
-    return 1;
+    return iptables_command("iptables -t nat -I prerouting_rule -m mac --mac-source %s -j RETURN",mac);
 }
